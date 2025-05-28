d23c5df6b2c465f95552723c19f7d69668bf98a0
unknown <logplayviews@gmail.com>
2025-05-28 13:46:07 -0300

    Feat: Product controller on WebUi layer implementation

--- CleanArchMvc.Application/DTOs/CategoryDTO.cs
+++ CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -10,9 +10,10 @@ namespace CleanArchMvc.Application.DTOs
     public class CategoryDTO
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "The name is required")]
+        [Required(ErrorMessage = "The Name is Required")]
         [MinLength(3)]
         [MaxLength(100)]
         public string Name { get; set; }
+
     }
 }
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -1,9 +1,4 @@
 ﻿using CleanArchMvc.Domain.Validation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
@@ -14,44 +9,55 @@ namespace CleanArchMvc.Domain.Entities
         public decimal Price { get; private set; }
         public int Stock { get; private set; }
         public string Image { get; private set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
 
         public Product(string name, string description, decimal price, int stock, string image)
         {
-            ValidationDomain(name, description, price, stock, image);
+            ValidateDomain(name, description, price, stock, image);
         }
 
         public Product(int id, string name, string description, decimal price, int stock, string image)
         {
-            DomainExceptionValidation.When(id < 0, "Invalid Id");
-            ValidationDomain(name, description, price, stock, image);
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
         }
 
         public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
         {
-            ValidationDomain(name, description, price, stock, image);
-            CategoryId = CategoryId;
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
         }
 
-        private void ValidationDomain(string name, string description, decimal price, int stock, string image)
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
         {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
 
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required.");
-            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description.Description is required.");
-            DomainExceptionValidation.When(name.Length < 3, "This name is too short, please add a name with 4 or more characters.");
-            DomainExceptionValidation.When(description.Length < 5, "This description is too short, please add a description with 5 or more characters.");
-            DomainExceptionValidation.When(price < 0, "this price is too low, please add 0 or higher price");
-            DomainExceptionValidation.When(stock < 0, "This stock is too low, please add 0 or higher stock");
-            DomainExceptionValidation.When(image?.Length > 250, "Invalid image. Image is too long.");
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
 
-            Description = description;
-            Image = image;
-            Stock = stock;
-            Price = price;
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+                "Invalid description. Description is required");
+
+            DomainExceptionValidation.When(description.Length < 5,
+                "Invalid description, too short, minimum 5 characters");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
+
+            DomainExceptionValidation.When(image?.Length > 250,
+                "Invalid image name, too long, maximum 250 characters");
+
             Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
 
         }
 
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
